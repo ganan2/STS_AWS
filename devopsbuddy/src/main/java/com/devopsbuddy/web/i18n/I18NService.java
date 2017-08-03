ef8bce0f90acc071a96c46a6b34aa8ef0316111d
ganan2 <ganan2@uis.edu>
2017-08-03 13:29:16 -0500

    Added logging setup

--- devopsbuddy/src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -2,6 +2,8 @@ package com.devopsbuddy.web.i18n;
 
 import java.util.Locale;
 
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -9,6 +11,9 @@ import org.springframework.stereotype.Service;
 
 @Service /** To tell Spring that this is a service */
 public class I18NService {
+	
+	/** The application Logger I18NService*/
+	private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(I18NService.class);
 	
 	/** Injection of messageSource bean using @Autowired. 
 	This tells Spring to look for an instance of MessageSource Interface */
@@ -20,6 +25,7 @@ public class I18NService {
 	 * @param messageId The key to the message resource file 
 	 **/
 	public String getMessage(String messageId) {
+		LOG.info("Returning i18n text for messageId {}", messageId);
 		Locale locale = LocaleContextHolder.getLocale();
 		return getMessage(messageId, locale);
 	}
