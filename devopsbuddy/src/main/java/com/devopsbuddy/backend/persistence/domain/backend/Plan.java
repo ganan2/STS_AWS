3c04d9e2600075e4228639455aacbf1f00767869
ganan2 <ganan2@uis.edu>
2017-08-08 21:48:47 -0500

    Create the JPA entity model for Spring Security

---
+++ devopsbuddy/src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Plan.java
@@ -0,0 +1,57 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Plan implements Serializable {
+	
+	private static final long serialVersionID = 1L;
+	
+	@Id
+	private int id;
+
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/** Default constructor 
+	 * Known default constructor isn't avilable to the JVM
+	 * Hence, for entities, this will cause an error 
+	 * Hence, more than one constructor is recommended */
+	public Plan() {
+		
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+		
+		Plan plan = (Plan) o;
+		
+		return id == plan.id;
+	}
+	
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+}
