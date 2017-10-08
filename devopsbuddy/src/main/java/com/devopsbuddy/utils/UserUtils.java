555f9783f00f0ed9630e99c74dcb99ff857a844b
ganan2 <ganan2@uis.edu>
2017-10-08 00:12:08 -0500

    created the frontend domain model and translation layer for basic account

--- devopsbuddy/src/main/java/com/devopsbuddy/utils/UserUtils.java
+++ devopsbuddy/src/main/java/com/devopsbuddy/utils/UserUtils.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.devopsbuddy.backend.persistence.domain.backend.User;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.domain.frontend.BasicAccountPayload;
 
 public class UserUtils {
 
@@ -23,10 +24,10 @@ public class UserUtils {
     public static User createBasicUser(String username, String email) {
 
         User user = new User();
-//        user.setUsername("username");
+        user.setUsername("username");
         user.setUsername(username);
         user.setPassword("secret");
-//        user.setEmail("me@example.com");
+        user.setEmail("me@example.com");
         user.setEmail(email);
         user.setFirstName("firstName");
         user.setLastName("lastName");
@@ -61,5 +62,20 @@ public class UserUtils {
                         token;
 
         return passwordResetUrl;
+    }
+
+    public static <T extends BasicAccountPayload> User fromWebUserToDomainUser(T frontendPayload) {
+        User user = new User();
+        user.setUsername(frontendPayload.getUsername());
+        user.setPassword(frontendPayload.getPassword());
+        user.setFirstName(frontendPayload.getFirstName());
+        user.setLastName(frontendPayload.getLastName());
+        user.setEmail(frontendPayload.getEmail());
+        user.setPhoneNumber(frontendPayload.getPhoneNumber());
+        user.setCountry(frontendPayload.getCountry());
+        user.setEnabled(true);
+        user.setDescription(frontendPayload.getDescription());
+
+        return user;
     }
 }
